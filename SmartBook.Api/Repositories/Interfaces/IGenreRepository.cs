b17e3f4b25c42c37c485d26bdef4dbdedc495028
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-04-17 15:45:04 +1000

    Commit 1: Organize Repository files into Interfaces and Implementations folders

--- SmartBook.Api/Repositories/Interfaces/IGenreRepository.cs
+++ SmartBook.Api/Repositories/Interfaces/IGenreRepository.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace SmartBook.Api.Repositories
 {
-    public interface IGenreRepository : IRepository<GenreR>
+    public interface IGenreRepository : IRepository<Genre>
     {
         // Add any specific methods for GenreR entity here if needed
     }
